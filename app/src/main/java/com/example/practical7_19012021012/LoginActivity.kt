42be951d97d00e404a6584fbc4e59e4a115594dc
Achal Hingrajiya <achalhingrajiya19@gnu.ac.in>
2021-09-22 15:46:02 +0530

    Partially designed login page

---
+++ app/src/main/java/com/example/practical7_19012021012/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practical7_19012021012
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
