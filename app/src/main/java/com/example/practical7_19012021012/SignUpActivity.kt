0733b35d69f643c354a9566896449b9d867b1f18
Achal Hingrajiya <achalhingrajiya19@gnu.ac.in>
2021-09-23 23:01:59 +0530

    Added buttons for login and signup in Login and Signup Activity resp

--- app/src/main/java/com/example/practical7_19012021012/SignUpActivity.kt
+++ app/src/main/java/com/example/practical7_19012021012/SignUpActivity.kt
@@ -1,11 +1,15 @@
 package com.example.practical7_19012021012
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
     }
 }
